b7cdbf81ba6d18d8b93917f857c3c32a099d07cb
ElvesSambo <elvessambo0@gmail.com>
2026-04-03 18:51:17 +0200

    First valiation try
    Added a function to validate names.

---
+++ ValidationPractice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+bool isName(std::string);
+
+int main()
+{
+	std::string name;
+	std::cout << "Insert a name here and wait for the validation: ";
+	std::cin >>name;
+return 0;
+}
+
+bool isName(std::string name)
+{
+	for(size_t i; i<name.length(); i++)
+		{
+			
+		}
+}
